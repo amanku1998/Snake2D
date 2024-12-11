b80d74b572eb8737d7b5089a91cae10a7e3954fb
Aman Kumar <aman.kumar01747@gmail.com>
2024-12-11 18:24:08 +0530

    -Add functionality to traverse the snake from the wall.

--- Assets/Script/Snake/SnakeController.cs
+++ Assets/Script/Snake/SnakeController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class SnakeController : MonoBehaviour
 {
-    public Transform segmentPrefab;
-    public Vector2Int direction = Vector2Int.right;
-    public float speed = 20f;
-    public float speedMultiplier = 1f;
-    public int initialSize = 4;
-    public bool moveThroughWalls = false;
+    [SerializeField] private Transform segmentPrefab;
+    [SerializeField] private Vector2Int direction = Vector2Int.right;
+    [SerializeField] private float speed = 20f;
+    [SerializeField] private float speedMultiplier = 1f;
+    [SerializeField] private int initialSize = 4;
+    [SerializeField] private bool moveThroughWalls = false;
 
-    private readonly List<Transform> snakeBodyPartList = new List<Transform>();
+    private readonly List<Transform> segmentOfSnakeBodyPartList = new List<Transform>();
     private Vector2Int input;
     private float nextUpdate;
 
@@ -65,9 +65,9 @@ public class SnakeController : MonoBehaviour
         // Set each segment's position to be the same as the one it follows. We
         // must do this in reverse order so the position is set to the previous
         // position, otherwise they will all be stacked on top of each other.
-        for (int i = snakeBodyPartList.Count - 1; i > 0; i--)
+        for (int i = segmentOfSnakeBodyPartList.Count - 1; i > 0; i--)
         {
-            snakeBodyPartList[i].position = snakeBodyPartList[i - 1].position;
+            segmentOfSnakeBodyPartList[i].position = segmentOfSnakeBodyPartList[i - 1].position;
         }
 
         // Move the snake in the direction it is facing
@@ -83,8 +83,8 @@ public class SnakeController : MonoBehaviour
     public void Grow()
     {
         Transform segment = Instantiate(segmentPrefab);
-        segment.position = snakeBodyPartList[snakeBodyPartList.Count - 1].position;
-        snakeBodyPartList.Add(segment);
+        segment.position = segmentOfSnakeBodyPartList[segmentOfSnakeBodyPartList.Count - 1].position;
+        segmentOfSnakeBodyPartList.Add(segment);
     }
 
     public void ResetState()
@@ -93,14 +93,14 @@ public class SnakeController : MonoBehaviour
         transform.position = Vector3.zero;
 
         // Start at 1 to skip destroying the head
-        for (int i = 1; i < snakeBodyPartList.Count; i++)
+        for (int i = 1; i < segmentOfSnakeBodyPartList.Count; i++)
         {
-            Destroy(snakeBodyPartList[i].gameObject);
+            Destroy(segmentOfSnakeBodyPartList[i].gameObject);
         }
 
         // Clear the list but add back this as the head
-        snakeBodyPartList.Clear();
-        snakeBodyPartList.Add(transform);
+        segmentOfSnakeBodyPartList.Clear();
+        segmentOfSnakeBodyPartList.Add(transform);
 
         // -1 since the head is already in the list
         for (int i = 0; i < initialSize - 1; i++)
@@ -108,5 +108,59 @@ public class SnakeController : MonoBehaviour
             Grow();
         }
     }
+    //
+    public bool Occupies(int x, int y)
+    {
+        foreach (Transform segment in segmentOfSnakeBodyPartList)
+        {
+            if (Mathf.RoundToInt(segment.position.x) == x &&
+                Mathf.RoundToInt(segment.position.y) == y)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            Grow();
+        }
+        else if (other.gameObject.CompareTag("Obstacle"))
+        {
+            ResetState();
+        }
+        else if (other.gameObject.CompareTag("Wall"))
+        {
+            if (moveThroughWalls)
+            {
+                Traverse(other.transform);
+            }
+            else
+            {
+                ResetState();
+            }
+        }
+    }
+
+    private void Traverse(Transform wall)
+    {
+        Vector3 position = transform.position;
+
+        if (direction.x != 0f)
+        {
+            position.x = Mathf.RoundToInt(-wall.position.x + direction.x);
+        }
+        else if (direction.y != 0f)
+        {
+            position.y = Mathf.RoundToInt(-wall.position.y + direction.y);
+        }
+
+        transform.position = position;
+    }
+
 
 }
